a645e815f54715f29f68d9213e01b4ced314d673
neha <neha.khan1@ontariotechu.net>
2025-08-03 04:18:53 -0400

    add letter hunt game

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,9 @@
 import HomePageScreen from "./screens/HomeScreen";
 import TestSelectionScreen from "./screens/TestSelectionScreen";
 import ResultsScreen from "./screens/ResultsScreen";
+import LetterHuntScreen from "./screens/LetterHuntScreen";
 
 
 export default function Index() {
-  return <ResultsScreen />;
+  return <LetterHuntScreen />;
 }
